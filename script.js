add24dc1add7c2c38a4af47032d8d1938af6b04f
Kristina <kgritsenko.2017@gmail.com>
2025-03-14 13:32:49 +0200

    final

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: 'premium'
 };
 
 const account2 = {
@@ -14,6 +14,8 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: 'premium'
+
 };
 
 const account3 = {
@@ -21,6 +23,8 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'standard'
+
 };
 
 const account4 = {
@@ -28,6 +32,8 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: 'basic'
+
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -58,9 +64,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const html = ` <div class="movements__row">
@@ -164,7 +173,7 @@ btnLoan.addEventListener('click', function (e) {
     inputLoanAmount.value = '';
   }
 }
-)
+);
 
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
@@ -179,4 +188,11 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 })
